77bed876c059b4d3d5005cc657715f52519aee9e
h3nnn4n <uber.renan@gmail.com>
2017-10-30 10:31:43 -0200

    now the coords are exported (as it should be) and not the angles

--- src/translate.py
+++ src/translate.py
@@ -10,14 +10,21 @@ names = sys.argv[1:]
 total_pose = 0
 pose_names = []
 
+plen = 0
+
 for name in names:
     pose = pyrosetta.pose_from_pdb(name)
+    plen = pose.total_residue()
     out = os.path.splitext(os.path.basename(name))[0] + '.tra1'
     pose_names.append(out)
     with open(out, 'wt') as f:
         f.write("%d 0.0 1 1\n" % pose.total_residue())
         for i in range(pose.total_residue()):
-            f.write("%10.5f %10.5f %10.5f\n" % (pose.phi(i + 1), pose.psi(i + 1), pose.omega(i + 1)))
+            for j in range(pose.residue(i + 1).natoms()):
+                if pose.residue(i + 1).atom(j + 1).type() == 24:
+                    coords = pose.residue(i + 1).atom(j + 1).xyz()
+                    f.write("%10.5f %10.5f %10.5f\n" % (coords[1], coords[2], coords[3]))
+                    break
 
     total_pose += 1
 
@@ -25,3 +32,7 @@ with open('tra.in', 'wt') as f:
     f.write("%d\n" % total_pose)
     for name in pose_names:
         f.write("%s\n" % name)
+
+with open('rmsinp', 'wt') as f:
+    f.write("%d %d\n" % (1, plen))
+    f.write("%d\n" % (plen))
